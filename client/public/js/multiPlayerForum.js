77b59c6daf7feea4e3307f3ecc84e2a9bc4b46f2
NickM <nmikstas@alumni.scu.edu>
2020-01-27 16:53:13 -0700

    Began basic layout of multiplayer forum

--- client/public/js/multiPlayerForum.js
+++ client/public/js/multiPlayerForum.js
@@ -1,1 +1,69 @@
-console.log("Multiplayer Forum");
+
+/**************************************** Resize Listener ****************************************/
+
+window.addEventListener("resize", () => 
+{
+    $('#pieceCanvas').width($('#pieceCanvas').parent().width());
+});
+
+/************************************** Disable Key Scrolling **************************************/
+
+window.addEventListener("keydown", (e) =>
+{
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1)
+    {
+        e.preventDefault();
+    }
+}, false);
+
+/************************************** Game Stats Callback **************************************/
+
+
+
+/***************************************** Game Handlers *****************************************/
+
+
+
+/*************************************** Button Listeners ****************************************/
+
+$(document).ready(() =>
+{
+    $(".leave-forum").on("click", () =>
+    {
+        window.location.href = "/home";
+    });
+});
+
+/*********************************** Game Engine And Renderer ************************************/
+
+let runForum = (data) =>
+{
+    
+}
+
+/******************************************* Top Level *******************************************/
+
+//Force page to reset if back or forward button is used.
+window.onunload = () => {};
+
+//Verify the user is logged in.
+$.post("/api/users/verify/")
+.then((data) =>
+{
+    //If not, boot 'em out!
+    if(data.notLoggedIn)
+    {
+        window.location.href = "/denied";
+    }
+
+    player = data.username;
+    highScore = data.highScore;
+    $(".user-title").text("Hello, " + data.username + "!");
+    runForum(data);
+})
+.fail(function(err)
+{
+    console.log(err);
+    window.location.href = "/denied";
+});

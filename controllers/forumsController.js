5f6e437fc90ea1d172d8dadc3c97c31ca702e663
Chris Mikstas <chrismikstas@gmail.com>
2020-01-26 17:53:08 -0700

    created first forums controller route

--- controllers/forumsController.js
+++ controllers/forumsController.js
@@ -1,5 +1,37 @@
 const db = require("../models");
 
+module.exports.forumHelper = (state, index, dbModel) =>
+{
+    switch (state)
+    {
+        case "pushOwner":
+            db.User.findOneAndUpdate({ username:  dbModel.owner},
+                { $push: { ownedForums: dbModel._id } }, { new: true })
+            .then(this.forumHelper("pushPlayer", 0, dbModel))
+            break;
+        case "pushPlayer":
+            if (dbModel.player2 === "")
+            {
+                this.forumHelper("pushSpectators", 0, dbModel)
+            }
+            else
+            {
+                db.User.findOneAndUpdate({ username:  dbModel.player2},
+                    { $push: { playerForums: dbModel._id } }, { new: true })
+                .then(this.forumHelper("pushSpectators", 0, dbModel))
+            }
+            break;
+        default:
+            if (index < dbModel.spectators.length)
+            {
+                db.User.findOneAndUpdate({ username:  dbModel.spectators[index].username},
+                    { $push: { otherForums: dbModel._id } }, { new: true })
+                .then(this.forumHelper("pushSpectators", ++index, dbModel))
+            }
+            break;
+    }
+}
+
 // Defining methods for the forumsController
 module.exports =
 {
@@ -8,7 +40,11 @@ module.exports =
     {
         //Try to create the new forum in the database.
         db.Forum.create(req.body)
-        .then(dbModel => res.json(dbModel))
+        .then(dbModel => 
+        {
+            res.json(dbModel);
+            this.forumHelper("pushOwner", 0, dbModel)
+        })
         .catch(err => res.status(422).json(err));
     },
 }

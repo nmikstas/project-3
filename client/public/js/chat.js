ac5ea29ca3ad2c2607a2771e05fcdd4d17002775
Chris Mikstas <chrismikstas@gmail.com>
2020-01-31 22:17:03 -0700

    added chat functionality to multiplayer forum

--- client/public/js/chat.js
+++ client/public/js/chat.js
@@ -1,6 +1,80 @@
 let player;
 let forumId;
 
+let isModerator = false;
+
+let databaseCommentsArr = [];
+let commentsArr = [];
+let lastComments = 0;
+
+let appendComments = () =>
+{
+    //console.log("hello");
+
+    $.get("/api/comments/getcomments/" + forumId)
+    .then((data) =>
+    {
+        //console.log(data);
+
+        databaseCommentsArr = [...data];
+
+        //console.log(databaseCommentsArr);
+    
+
+        if (commentsArr !== databaseCommentsArr)
+        {
+            for (let i = lastComments; i < databaseCommentsArr.length; i++)
+            {
+                commentsArr.push(databaseCommentsArr[i]);
+            }
+        }
+
+        if (lastComments !== commentsArr.length)
+        {
+            //console.log(commentsArr);
+
+            for (let i = lastComments; i < commentsArr.length; i++)
+            {
+                let userName = commentsArr[i].username;
+                let userNameSpan = $("<span>");
+                userNameSpan.addClass("chat-username");
+                userNameSpan.append("<b>" + userName + "<b>" + ": ");
+
+                let userCommentId = commentsArr[i]._id;
+                //console.log(userCommentId);
+
+                let userComment = commentsArr[i].comment;
+                let userCommentSpan = $("<span>");
+                userCommentSpan.addClass("chat-usertext");
+                userCommentSpan.attr("id", userCommentId)
+                userCommentSpan.append(userComment);
+
+                let commentDiv = $("<div>");
+                commentDiv.addClass("mx-2 text-left");
+
+                commentDiv.append(userNameSpan);
+                commentDiv.append(userCommentSpan);
+
+                if (isModerator)
+                {
+                    let moderateCommentIcon = "X";
+                    let moderateCommentBtn = $("<button>");
+                    let moderateCommentDiv = $("<div>");
+                    moderateCommentBtn.addClass("deleteCommentBtn");
+                    moderateCommentBtn.attr("type", "button");
+
+                    moderateCommentBtn.append(moderateCommentIcon);
+                    moderateCommentDiv.append(moderateCommentBtn);
+                    commentDiv.append(moderateCommentDiv);
+                }
+
+                $(".chat-div").append(commentDiv);
+            }
+        }
+    });
+    lastComments = commentsArr.length;
+}
+
 $(document).ready(() =>
 {
     $("#chat-btn").on("click", function (event)
@@ -38,11 +112,31 @@ $.post("/api/users/verify/")
     player = data.username;
     forumId = data.targetForum;
 
-    $.get("/api/comments/getcomments/" + forumId)
-    .then((data) =>
+    //Get the forum data.
+    $.get("/api/forums/getforum/" + forumId)
+    .then(forumData =>
     {
-        console.log(data);
+        //console.log(forumData);
+        if (player === forumData.owner)
+        {
+            //console.log("forum owner: " + forumData.owner);
+            isModerator = true;
+        }
+
+        for (let i = 0; i < forumData.spectators.length; i++)
+        {
+            if (forumData.spectators[i].username === player)
+            {
+                isModerator = forumData.spectators[i].isModerator;
+            }
+        }
+
+        //console.log("player: " + player + ", is moderator: " + isModerator);
+
+        appendComments();
+        setInterval(appendComments, 1000);
     });
+
 })
 .fail(function(err)
 {

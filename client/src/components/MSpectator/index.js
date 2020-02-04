b25902927cb02c0867c11bd2f1c6f0f7cb142150
Chris Mikstas <chrismikstas@gmail.com>
2020-02-03 21:42:30 -0700

    changed 2nd page of create forum page to be more user friendly and remove information from different user boxes.

--- client/src/components/MSpectator/index.js
+++ client/src/components/MSpectator/index.js
@@ -5,7 +5,7 @@ const MSpectator = (props) =>
 {
     return (
         <div>
-            <button data-user={props.username} onClick={props.deleteSpectator} className="deleteSpectator ml-1">X</button>
+            <button data-user={props.username} data-index={props.index} onClick={props.deleteSpectator} className="deleteSpectator ml-1">X</button>
             <button data-index={props.index} onClick={props.changeToSpectator} className="addModerator ml-1 mr-2">M</button>
             <span className="text-white">{props.username}</span>
         </div>
